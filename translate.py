9ca27358914538bf7e8cc70fefe83176a9408aa6
toushangyouxiang <2375741859@qq.com>
2018-08-07 21:46:58 +0800

    大致完成

--- translate.py
+++ translate.py
@@ -34,24 +34,29 @@ language_list = """{
 "vie":"越南语"
 }"""
 
-guide = """python3 translate.py -h\n==>help\n
-python3 translate.py -c\n==>command line mode\n
-python3 translate.py -d -q <text>\n==>default mode, translate text to Simplified Chinese\n
-python3 translate.py -o -f <language code a> -t <language code b> -q <text>\n==>translate text from language a to b\n
-python3 translate.py list\n==>show the language code list
+guide = """python3 translate.py -h\n==>帮助\n
+python3 translate.py -c\n==>命令行模式\n
+python3 translate.py -d -q <文本>\n==>将任何语言的文本翻译成中文\n
+python3 translate.py -o -f <语言代码 a> -t <语言代码 b> -q <text>\n==>把文本从语言 a 翻译为语言 b\n
+python3 translate.py list\n==>显示语言代码
+
+为避免不必要的麻烦,参数请务必加引号!
+
+示例:将 "I swear I lived" 翻译为繁体中文:python translate.py -o -f "en" -t "cht" -q "I swear I lived"
 """
 
 cmd_guide = """
-@from <language code>
-@to <language>
-<text> translate 
+@from <语言代码>     更改原文语言
+@to <语言代码>       更改译文语言
+@exit               退出
+
 """
 
 
 # ordinary mode
 def ordinary(f, t, q):
     data = translate(q, f, t)
-    content = f+"->"+t+"\n"+data["dst"]
+    content = json.loads(language_list)[f]+"->"+json.loads(language_list)[t]+"\n"+data["dst"]
     return content
 
 
@@ -64,10 +69,13 @@ def cmd():
         inp = input(">")
         if "@from" == inp.split(" ")[0]:
             f = inp.split(" ")[1]
+            print("原文语言已变更为:"+json.loads(language_list)[f])
         elif "@to" == inp.split(" ")[0]:
             t = inp.split(" ")[1]
+            print("译文语言已变更为:"+json.loads(language_list)[t])
         elif "@exit" == inp.split(" ")[0]:
             sys.exit(0)
+            print("已退出")
         else:
             print(translate(inp, f, t)["dst"])
 
@@ -87,12 +95,18 @@ def lang():
 # arg explain
 args = sys.argv
 if len(args) == 1:
-    print("No arg,exit")
+    print("没有指令")
     sys.exit(0)
 else:
     if args[1] == "-o":
+        if not (args[2] == "-f" and args[4] == "-t" and args[6 == "-q"]):
+            print("指令有误")
+            sys.exit(0)
         print(ordinary(args[3], args[5], args[7]))
     elif args[1] == "-d":
+        if not (args[2] == "-q"):
+            print("指令有误")
+            sys.exit(0)
         print(default(args[3]))
     elif args[1] == "-c":
         cmd()
@@ -101,4 +115,4 @@ else:
     elif args[1] == "list":
         lang()
     else:
-        print("wrong")
+        print("指令有误")
